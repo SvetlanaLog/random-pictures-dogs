7040457c5b3a03711ccb8920cdafd9c66bdacdf8
Светлана <sve-bashmakova@ya.ru>
2022-10-19 22:33:11 +0300

    fix

--- readme.js
+++ readme.js
@@ -1,5 +1,17 @@
-let getPictureDog = function (){
-    console.log('Test');
+const DogsUrl = 'https://dog.ceo/api/breeds/image/random';
+const picturesВogs= document.querySelector('.img-dogs');
+const changePictureDog = async function (){
+    let response = await fetch(DogsUrl);
+
+    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+      // получаем тело ответа (см. про этот метод ниже)
+      const json = await response.json();
+      const message = json.message;
+      picturesВogs.src = message
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+
 };
-let elem = document.getElementById('button-1');
-elem.addEventListener('click', getPictureDog)
+const elem = document.getElementById('button-1');
+elem.addEventListener('click', changePictureDog)
